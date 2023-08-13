59f4231d2dc9903320ed0ba2ccee763b45ebdd39
tensorturtle <tensorturtle@gmail.com>
2023-08-13 14:45:07 -0900

    updates

--- velovision-pi-cam/src/main.rs
+++ velovision-pi-cam/src/main.rs
@@ -1,45 +1,80 @@
-use tiny_http::{Server, Response};
+use std::error::Error;
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use std::net::TcpStream;
 
-fn main() {
-    let server: Server = Server::http("0.0.0.0:8000").unwrap();
+use rppal::gpio::Gpio;
 
-    for mut request in server.incoming_requests() {
-        println!("received request! method: {:?}, url: {:?}, headers: {:?}",
-            request.method(),
-            request.url(),
-            request.headers()
-        );
+use systemctl;
 
-        let mut response = Response::from_string("hello world");
+pub fn check_tcp_service(port: u16) -> bool {
+    let addr = format!("localhost:{}", port);
+    match TcpStream::connect(addr) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
 
+struct CameraStreamStatus {
+    systemd_active: bool,
+    tcp_stream_exists: bool,
+}
 
-        // GET request example
-        // Test with: curl 192.168.9.1:8000
-        if request.method() == &tiny_http::Method::Get {
-            // Get url
-            let url = request.url();
-            if url == "/" {
-                println!("Root request");
-                response = Response::from_string("hello root");
-            } else {
-                println!("Non-root request");
-                response = Response::from_string("hello non-root");
-            }
-        }
+fn main() -> Result<(), Box<dyn Error>> {
+    let gpio = Gpio::new().unwrap();
+    let mut pin = match gpio.get(21) {
+        Ok(pin) => pin.into_output(),
+        Err(e) => panic!("Error getting GPIO pin: {}", e),
+    };
 
-        // POST request example
-        // Test with: curl -X POST -d "jason" 192.168.9.1:8000
-        if request.method() == &tiny_http::Method::Post {
-            // Get post content
-            let mut post_content = String::new();
-            request.as_reader().read_to_string(&mut post_content).unwrap();
-            println!("POST content: {}", post_content);
-
-            let reply_content = format!("hello {}", post_content);
-
-            response = Response::from_string(reply_content);
-        }
-        request.respond(response);
+    loop {
+        println!("Setting pin to high");
+        pin.set_high();
+        thread::sleep(Duration::from_millis(100));
+        println!("Setting pin to low");
+        pin.set_low();
+        thread::sleep(Duration::from_millis(100));
+        pin.set_high();
     }
 
+
+    // println!("Re-starting camera systemd service");
+    // systemctl::restart("camera-mjpeg-over-tcp.service").unwrap();
+
+    // let camera_stream_status = Arc::new(
+    //     Mutex::new(
+    //         CameraStreamStatus {
+    //             systemd_active: false,
+    //             tcp_stream_exists: false,
+    //         }
+    //     )
+    // );
+
+    // // just loop this active check
+    // loop {
+    //     let is_active = systemctl::is_active("camera-mjpeg-over-tcp.service").unwrap();
+    //     println!("Systemd service is active: {}", is_active);
+
+    //     let tcp_service_exists = check_tcp_service(5000);
+    //     println!("TCP stream exists: {}", tcp_service_exists);
+
+    //     println!("\n");
+    //     thread::sleep(Duration::from_secs(1));
+    // }
+    // println!("Stopping service");
+    // systemctl::stop("camera-mjpeg-over-tcp.service").unwrap();
+
+
+    // let mut pin = gpio.get(3).unwrap().into_input_pullup();
+    // loop {
+    //     if pin.is_high() {
+    //         println!("3: High")
+    //     } else {
+    //         println!("3: Low")
+    //     }
+    //     thread::sleep(Duration::from_millis(100));
+    // }
+    Ok(())
 }
+
